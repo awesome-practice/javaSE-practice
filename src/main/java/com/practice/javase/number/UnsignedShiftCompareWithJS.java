7b295e6e7cea3ea0f7714cf64b3ca7f3361d35d8
LeonGeroge <leongeroge@gmail.com>
2022-01-01 17:29:09 +0800

    UnsignedShiftCompareWithJS

--- src/main/java/com/practice/javase/number/UnsignedShiftCompareWithJS.java
+++ src/main/java/com/practice/javase/number/UnsignedShiftCompareWithJS.java
@@ -5,7 +5,7 @@ public class UnsignedShiftCompareWithJS
 	public static void main(String[] args) {
 	    int i=-100;
 	    int j=i>>>0;
-	    System.out.println(j);
+	    System.out.println(j);//JS: -100>>>0 : 4294967196
 
 		System.out.println(Integer.toUnsignedLong(j));//4294967196
 	}
